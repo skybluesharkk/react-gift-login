68f5d440a50c8a8b8c2a35b5427d3e5b53b07bb7
skybluesharkk <104782301+skybluesharkk@users.noreply.github.com>
2025-06-24 19:35:45 +0900

    부산대 FE 심영찬 - 1단계 프로젝트 세팅 (#48)
    * docs: update README
    
    * chore: 불필요한 파일,코드를 정리하기
    
    * build: 절대경로로 import할 수 있게 alias를 설정하기
    
    * style: Prettier를 추가하기
    
    * feat: emotion 스타일 라이브러리를 추가하고 reset css를 적용하기
    
    * feat: 기본 폰트로 Pretendard를 적용하기

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react-gift-login
+1. 불필요한 파일, 코드를 정리하기
+2. 절대경로로 Import 할 수 있게 alias를 설정하기
+3. Prettier가 무엇인지 찾아보고 추가하기
+4. emotion 스타일 라이브러리를 추가하고, reset css를 적용하기
+5. 기본 폰트로 Pretendard를 적용하기
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import App from "@/App"
+import GlobalStyle from "./styles/GlobalStyle"
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <GlobalStyle />
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
